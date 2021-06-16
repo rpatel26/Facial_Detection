ea916210c5c6903eea1858f996c044dd04e5aef7
Ravi Patel <rdp2606@gmail.com>
2021-06-16 19:05:44 -0400

    haarcascade model for eye and facial detection

--- Models.py
+++ Models.py
@@ -1,13 +1,82 @@
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, GlobalAveragePooling2D, SpatialDropout2D
-from keras.optimizers import SGD
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, GlobalAveragePooling2D, SpatialDropout2D
+# from keras.optimizers import SGD
+import cv2
+import numpy as np
 
 class Models(object):
 	"""docstring for Models"""
-	def __init__(self, ):
+	def __init__(self):
+		self.eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+		self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')	
 		pass
 
 	def build_model(self):
-		model = Sequential()
+		pass
 		
-		return model	
+		# model = Sequential()
+		# model.add(Conv2D(32, (3,3), padding='same', activation='relu', kernel_initializer='he_normal', input_shape=(96,96,1)))
+		# model.add(MaxPool2D(pool_size=(2, 2)))
+		# model.add(SpatialDropout2D(0.25))
+		# model.add(GlobalAveragePooling2D())
+		# model.add(Dense(256, activation='relu', kernel_initializer="he_normal"))
+		# model.add(Dropout(0.5))
+		# model.add(Dense(30, activation='sigmoid'))
+
+		# sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+		# model.compile(loss='mean_squared_error', optimizer=sgd)	
+		# return model
+
+	def haarcascade_eye_detector(self, gray_image, rgb_image):
+		assert isinstance(gray_image, np.ndarray)
+		assert isinstance(rgb_image, np.ndarray)
+		assert len(gray_image.shape) == 2
+		assert len(rgb_image.shape) == 3
+
+		face_loc = self.face_cascade.detectMultiScale(gray_image, scaleFactor = 1.1, minNeighbors = 5)
+
+		for x, y, dx, dy in face_loc:
+			face_img_gray = gray_image[y:y+dy, x:x+dx]
+			face_img_rgb = rgb_image[y:y+dy, x:x+dx]
+			
+			eye_loc = self.eye_cascade.detectMultiScale(face_img_gray, scaleFactor = 1.05, minNeighbors = 5)
+
+			for ey_x, ey_y, ey_dx, ey_dy in eye_loc:
+				cv2.rectangle(face_img_rgb, (ey_x, ey_y), (ey_x + ey_dx, ey_y + ey_dy), (0, 255, 0), 3)
+
+
+	def haarcascade_face_detector(self, gray_image, rgb_image):
+		assert isinstance(gray_image, np.ndarray)
+		assert isinstance(rgb_image, np.ndarray)
+		assert len(gray_image.shape) == 2
+		assert len(rgb_image.shape) == 3
+
+		face_loc = self.face_cascade.detectMultiScale(gray_image, scaleFactor = 1.1)
+		for x, y, dx, dy in face_loc:
+			cv2.rectangle(rgb_image, (x, y), (x + dx, y + dy), (255, 0, 0), 3)	
+
+	def haarcascade_eye_and_face_detector(self, gray_image, rgb_image):
+		assert isinstance(gray_image, np.ndarray)
+		assert isinstance(rgb_image, np.ndarray)
+		assert len(gray_image.shape) == 2
+		assert len(rgb_image.shape) == 3
+
+		face_loc = self.face_cascade.detectMultiScale(gray_image, scaleFactor = 1.1, minNeighbors = 5)
+
+		for x, y, dx, dy in face_loc:
+			cv2.rectangle(rgb_image, (x, y), (x + dx, y + dy), (255, 0, 0), 3)
+			face_img_gray = gray_image[y:y+dy, x:x+dx]
+			face_img_rgb = rgb_image[y:y+dy, x:x+dx]
+			
+			eye_loc = self.eye_cascade.detectMultiScale(face_img_gray, scaleFactor = 1.05, minNeighbors = 5)
+
+			for ey_x, ey_y, ey_dx, ey_dy in eye_loc:
+				cv2.rectangle(face_img_rgb, (ey_x, ey_y), (ey_x + ey_dx, ey_y + ey_dy), (0, 255, 0), 3)
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Dataset import Dataset
 from ComputerVision import ComputerVision
-# from Models import Models
+from Models import Models
+import cv2
 
 def main():
 	print("Main...")
@@ -14,15 +15,38 @@ def main():
 	print("Xtest: ", Xtest.shape)
 	print("Ytest: ", Ytest.shape)
 
-	
+
 
 	# cv = ComputerVision()
 	# img = cv.mark_center(images[0,:,:], landmarks["left_eye_center_x"][0], landmarks["left_eye_center_y"][0])
 	# cv.show_image(img)
 
-	# model = Models()
+	model = Models()
 	# keypoint_model = model.build_model()
-	# print("model: ", keypoint_model)
+	# print("model: ", keypoint_model.summary())
+
+	cap = cv2.VideoCapture(0)
+	eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+	face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+	while True:
+		ret, frame = cap.read()
+
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# model.haarcascade_eye_detector(gray, frame)
+		model.haarcascade_face_detector(gray, frame)
+		# model.haarcascade_eye_and_face_detector(gray, frame)
+
+		cv2.imshow('frame', frame)
+
+		if cv2.waitKey(1) == ord('q'):
+			break
+
+
+
+	cap.release()
+	cv2.destroyAllWindows()	
 
 
 
